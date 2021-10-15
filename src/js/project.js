9c6210f431937c656b3d8faafc91ff76f55f8c97
jihwa <mungsome@gmail.com>
2021-10-15 17:15:08 +0900

    js 파일수정중
    CDN 문제아닌가배

--- src/js/project.js
+++ src/js/project.js
@@ -1,12 +1,5 @@
 
-
-
 $(document).ready(function(){
-
-  
-
-
-
 
   // 스와이퍼
 
@@ -29,45 +22,45 @@ $(document).ready(function(){
 
 
 
-  swiper.on('slideChange', function () {
-    switch (this.activeIndex) {
-      case 0 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat OldLace", "background-size": "cover"});
-      $(".txt").css({"color":"#355351"});         
-      $(".swiper-pagination-bullet-active").css({"background":"#355351"});         
-      $(".title-media720").css({"color":"#355351"});         
-      break;
-      case 1 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat #aba5a1", "background-size": "cover"}); 				          
-      $(".txt").css({"color":"#5b3220"});				          
-      $(".swiper-pagination-bullet-active").css({"background":"#5b3220"});         
-      $(".title-media720").css({"color":"#5b3220"});         
-      break;
-      case 2 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat blanchedalmond", "background-size": "cover"}); 				          
-      $(".txt").css({"color":"gray"});				          
-      $(".swiper-pagination-bullet-active").css({"background":"gray"});         
-      $(".title-media720").css({"color":"gray"});         
-      break;
-      case 3 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat beige", "background-size": "cover"}); 				          
-      $(".txt").css({"color":"black"});				          
-      $(".swiper-pagination-bullet-active").css({"background":"black"});         
-      $(".title-media720").css({"color":"black"});         
-      break; 
-    }
-  });
+  // swiper.on('slideChange', function () {
+  //   switch (this.activeIndex) {
+  //     case 0 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat OldLace", "background-size": "cover"});
+  //     $(".txt").css({"color":"#355351"});         
+  //     $(".swiper-pagination-bullet-active").css({"background":"#355351"});         
+  //     $(".title-media720").css({"color":"#355351"});         
+  //     break;
+  //     case 1 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat #aba5a1", "background-size": "cover"}); 				          
+  //     $(".txt").css({"color":"#5b3220"});				          
+  //     $(".swiper-pagination-bullet-active").css({"background":"#5b3220"});         
+  //     $(".title-media720").css({"color":"#5b3220"});         
+  //     break;
+  //     case 2 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat blanchedalmond", "background-size": "cover"}); 				          
+  //     $(".txt").css({"color":"gray"});				          
+  //     $(".swiper-pagination-bullet-active").css({"background":"gray"});         
+  //     $(".title-media720").css({"color":"gray"});         
+  //     break;
+  //     case 3 : $(".background").css({"background":"url(../img/tt4.png) center top no-repeat beige", "background-size": "cover"}); 				          
+  //     $(".txt").css({"color":"black"});				          
+  //     $(".swiper-pagination-bullet-active").css({"background":"black"});         
+  //     $(".title-media720").css({"color":"black"});         
+  //     break; 
+  //   }
+  // });
   
 
 
 
 
   // 페이지네이션 추가
-  $("<span>WOOAMI 브랜드 리뉴얼</span>").appendTo(".swiper-pagination-bullet:nth-of-type(1)");
+  // $("<span>WOOAMI 브랜드 리뉴얼</span>").appendTo(".swiper-pagination-bullet:nth-of-type(1)");
 
-  $("<span>LOCK N LOCK 클론 코딩</span>").appendTo(".swiper-pagination-bullet:nth-of-type(2)");
+  // $("<span>LOCK N LOCK 클론 코딩</span>").appendTo(".swiper-pagination-bullet:nth-of-type(2)");
 
-  $("<span>REACT 작업물</span>").appendTo(".swiper-pagination-bullet:nth-of-type(3)");
+  // $("<span>REACT 작업물</span>").appendTo(".swiper-pagination-bullet:nth-of-type(3)");
 
-  $("<span>SNS INSTAGRAM 클론 코딩</span>").appendTo(".swiper-pagination-bullet:nth-of-type(4)");
+  // $("<span>SNS INSTAGRAM 클론 코딩</span>").appendTo(".swiper-pagination-bullet:nth-of-type(4)");
 
-  $("<span>다번째 작업</span>").appendTo(".swiper-pagination-bullet:nth-of-type(5)");
+  // $("<span>다번째 작업</span>").appendTo(".swiper-pagination-bullet:nth-of-type(5)");
 
 
 
@@ -76,38 +69,34 @@ $(document).ready(function(){
 
   // $('.swiper-slide').css({'transform':'rotate('+test0+'deg)'})
   
-  $('.swiper-pagination').mouseenter(function(){
-    $('.swiper-slide').css(
-      {'transform':'scale(80%)',
-      'transform':'translateX(-20%)'})
-    // $('.img-swiper').css({'transform':'scale(80%)'})
-    $('.background2').css("display" ,"block");
-    $('.txt').css("display","none");
-    $('.swiper-pagination-bullet span').css("display","block");
-    $('.swiper-pagination').css("width","200px");
-    $('.swiper-pagination-bullet').css("background","white")
-  })
+  // $('.swiper-pagination').mouseenter(function(){
+  //   $('.swiper-slide').css(
+  //     {'transform':'scale(80%)',
+  //     'transform':'translateX(-20%)'})
+  //   $('.background2').css("display" ,"block");
+  //   $('.txt').css("display","none");
+  //   $('.swiper-pagination-bullet span').css("display","block");
+  //   $('.swiper-pagination').css("width","200px");
+  //   $('.swiper-pagination-bullet').css("background","white")
+  // })
 
 
 
-  $('.swiper-pagination').mouseleave(function(){
-    $('.swiper-slide').css(
-      {'transform':'scale(100%)',
-      'transform':'translateX(0%)'})
-    // $('.img-swiper').css({'transform':'scale(80%)'})
-    $('.background2').css("display" ,"none");
-    $('.txt').css("display","block");
-    $('.swiper-pagination-bullet span').css("display","none");
-    $('.swiper-pagination').css("width","45px");
-    $('.swiper-pagination-bullet').css("background","rgba(0,0,0,0.8");
-    $('.swiper-pagination-bullet-active').css("background","gray")
+  // $('.swiper-pagination').mouseleave(function(){
+  //   $('.swiper-slide').css(
+  //     {'transform':'scale(100%)',
+  //     'transform':'translateX(0%)'})
+  //   $('.background2').css("display" ,"none");
+  //   $('.txt').css("display","block");
+  //   $('.swiper-pagination-bullet span').css("display","none");
+  //   $('.swiper-pagination').css("width","45px");
+  //   $('.swiper-pagination-bullet').css("background","rgba(0,0,0,0.8");
+  //   $('.swiper-pagination-bullet-active').css("background","gray")
 
-  })
+  // })
 
-  if (window.matchMedia('(max-width: 720px)').matches)
-  {
-      console.log('720?')
-  }
+
+  // 반응형할 경우
   
 
 })
@@ -148,6 +137,3 @@ $(document).ready(function(){
 //   })
 // });
 
-
-
-// 커서
